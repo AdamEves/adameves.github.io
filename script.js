31997fe33ec4b1b495711e8b38f3a4bb4e42133b
AdamEves <98350132+AdamEves@users.noreply.github.com>
2023-10-20 15:41:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-// script.js
-function showDesign(imagePath) {
-  document.getElementById('popup-image').src = imagePath;
-  document.getElementById('popup').classList.remove('hidden');
-}
-
-function closePopup() {
-  document.getElementById('popup').classList.add('hidden');
+function addImage() {
+  const fileInput = document.getElementById('imageFile');
+  const file = fileInput.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function(e) {
+      const imgElement = document.createElement('img');
+      imgElement.src = e.target.result;
+      imgElement.alt = 'Dynamically Added Image';
+      imgElement.width = 150; // Thumbnail width
+      imgElement.onclick = function() {
+        showImage(e.target.result);
+      };
+      document.getElementById('gallery').appendChild(imgElement);
+    };
+  }
 }
